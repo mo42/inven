c9b40324d7734deb9ab1967f48540e1e1107bd0f
Moritz Winter <me@mo42.de>
2023-09-03 22:34:37 +0200

    One should be able to capture date of items

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,11 @@ main :: IO ()
 main = do
   args <- getArgs
   case args of
-    ("add" : "-d" : description : "-v" : valueStr : []) -> do
+    ("add" : "-t" : description : "-v" : valueStr : "-d" : dateStr : []) -> do
       let value = read valueStr :: Float
       inventory <- loadInventory
-      let newInventory = addItem description value inventory
+      date <- parseDateOrCurrent dateStr
+      let newInventory = addItem description value date inventory
       saveInventory newInventory
 
     ("remove" : itemIDStr : []) -> do
