c01784fd5dcd28d023693513a49954e515e640e4
Moritz Winter <me@mo42.de>
2024-01-07 23:07:23 +0100

    Use EDITOR env. variable

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,13 @@
 module Main (main) where
 
-import System.Process
-import System.Exit
 import Lib
+import System.Environment
+import System.Exit
+import System.Process
+
+getEditor :: Maybe String -> String
+getEditor (Just editor) = editor
+getEditor Nothing = "vi"
 
 main :: IO ()
 main = do
@@ -25,12 +30,16 @@ main = do
       putStrLn $ show $ count inventory
     Edit itemId -> do
       path <- appendToPath "inventory.yml"
-      let command = "nvim +/\"id: " ++ show itemId ++ "\" " ++ path
+      maybeEditor <- lookupEnv "EDITOR"
+      let command = getEditor maybeEditor ++ " " ++ path
       (_, _, _, processHandle) <- createProcess (shell command)
       exitCode <- waitForProcess processHandle
       case exitCode of
-        ExitSuccess -> putStrLn $ "Opened file at id: " ++ show itemId
-        ExitFailure _ -> putStrLn "Could not open inventory in NeoVim"
+        ExitSuccess -> do
+          exitSuccess
+        ExitFailure _ -> do
+          putStrLn "Could not open inventory in editor"
+          exitFailure
     Consume itemId -> do
       inventory <- loadInventory
       let newInventory = consume inventory itemId
