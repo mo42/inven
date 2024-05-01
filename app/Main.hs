1248d2f3b09127b5355e79efe8557068c871555f
Moritz Winter <me@mo42.de>
2024-05-01 16:25:15 +0200

    Format entire project source

--- app/Main.hs
+++ app/Main.hs
@@ -10,45 +10,45 @@ import Text.Printf
 
 main :: IO ()
 main = do
-    inventory <- loadInventory
-    parsedArgs <- getParsedArgs
-    case parsedArgs of
-        Add description itemDate quantity itemValue itemPrice itemCategory itemContainer itemLocation itemExpiry -> do
-            date <- parseDateOrCurrent itemDate
-            let newInventory = addItem description itemValue itemPrice date quantity itemCategory itemContainer itemLocation itemExpiry inventory
-            saveInventory newInventory
-        Remove itemId -> do
-            let newInventory = removeItem itemId inventory
-            saveInventory newInventory
-        Value -> do
-            print (totalValue inventory)
-        Count -> do
-            print (length inventory)
-        Edit -> do
-            path <- appendToPath "inventory.yml"
-            maybeEditor <- lookupEnv "EDITOR"
-            let command = fromMaybe "vi" maybeEditor ++ " " ++ path
-            (_, _, _, processHandle) <- createProcess (shell command)
-            exitCode <- waitForProcess processHandle
-            case exitCode of
-                ExitSuccess -> do
-                    exitSuccess
-                ExitFailure _ -> do
-                    putStrLn "Could not open inventory in editor"
-                    exitFailure
-        Consume itemId -> do
-            let newInventory = consume inventory itemId
-            saveInventory newInventory
-        Prune -> do
-            let newInventory = prune inventory
-            saveInventory newInventory
-            printf "Pruned %d items with zero quantity.\n" (length inventory - length newInventory)
-        Show itemId -> do
-            let maybeItem = findItemById inventory itemId
-            case maybeItem of
-                Just item -> putStr $ formatItem item
-                Nothing -> putStrLn "Not found"
-        Find regex -> do
-            let matchedItems = findItemsByRegex regex inventory
-            putStrLn headerLine
-            putStr $ intercalate "" $ map formatItemShort matchedItems
+  inventory <- loadInventory
+  parsedArgs <- getParsedArgs
+  case parsedArgs of
+    Add description itemDate quantity itemValue itemPrice itemCategory itemContainer itemLocation itemExpiry -> do
+      date <- parseDateOrCurrent itemDate
+      let newInventory = addItem description itemValue itemPrice date quantity itemCategory itemContainer itemLocation itemExpiry inventory
+      saveInventory newInventory
+    Remove itemId -> do
+      let newInventory = removeItem itemId inventory
+      saveInventory newInventory
+    Value -> do
+      print (totalValue inventory)
+    Count -> do
+      print (length inventory)
+    Edit -> do
+      path <- appendToPath "inventory.yml"
+      maybeEditor <- lookupEnv "EDITOR"
+      let command = fromMaybe "vi" maybeEditor ++ " " ++ path
+      (_, _, _, processHandle) <- createProcess (shell command)
+      exitCode <- waitForProcess processHandle
+      case exitCode of
+        ExitSuccess -> do
+          exitSuccess
+        ExitFailure _ -> do
+          putStrLn "Could not open inventory in editor"
+          exitFailure
+    Consume itemId -> do
+      let newInventory = consume inventory itemId
+      saveInventory newInventory
+    Prune -> do
+      let newInventory = prune inventory
+      saveInventory newInventory
+      printf "Pruned %d items with zero quantity.\n" (length inventory - length newInventory)
+    Show itemId -> do
+      let maybeItem = findItemById inventory itemId
+      case maybeItem of
+        Just item -> putStr $ formatItem item
+        Nothing -> putStrLn "Not found"
+    Find regex -> do
+      let matchedItems = findItemsByRegex regex inventory
+      putStrLn headerLine
+      putStr $ intercalate "" $ map formatItemShort matchedItems
